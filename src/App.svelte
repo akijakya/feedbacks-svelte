d1727b7b2740c54ef8e5a6b5783007a1118569f5
Andras Jaky <jakyandras@gmail.com>
2021-11-07 21:50:30 +0100

    feat: cards with delete function

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,33 @@
 <script lang="ts">
-	export let name: string;
+	import FeedbackList from "./components/FeedbackList.svelte";
+
+	let feedback: {id: number; rating: number; text: string}[] = [
+		{
+			id: 1,
+			rating: 8,
+			text: "lorem ipsum, vágod hapsim"
+		},
+		{
+			id: 2,
+			rating: 2,
+			text: "lorem ipsum, vágod hapsim"
+		},
+		{
+			id: 3,
+			rating: 7,
+			text: "lorem ipsum, vágod hapsim"
+		},
+	]
+
+	const deleteFeedback = (e) => {
+		const itemId = e.detail;
+		feedback = feedback.filter((item) => item.id != itemId);
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="container">
+	<FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
