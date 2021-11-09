002824aa9df3244712724d19c2f166da9b8def69
Andras Jaky <jakyandras@gmail.com>
2021-11-09 22:28:49 +0100

    feat: add rating counter and input field

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import FeedbackList from "./components/FeedbackList.svelte";
+	import FeedbackStats from "./components/FeedbackStats.svelte";
+	import FeedbackForm from "./components/FeedbackForm.svelte";
 
 	let feedback: {id: number; rating: number; text: string}[] = [
 		{
@@ -19,6 +21,9 @@
 		},
 	]
 
+	$: count = feedback.length;
+	$: average = feedback.reduce((a, {rating}) => a + rating, 0) / feedback.length
+
 	const deleteFeedback = (e) => {
 		const itemId = e.detail;
 		feedback = feedback.filter((item) => item.id != itemId);
@@ -26,6 +31,8 @@
 </script>
 
 <main class="container">
+	<FeedbackForm></FeedbackForm>
+	<FeedbackStats {count} {average}/>
 	<FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
 </main>
 
